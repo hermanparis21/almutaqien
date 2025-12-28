8da71a5478d723b0b7b1c933feaec4f3f4cd78d2
hermanparis21 <herman.paris21@gmail.com>
2025-12-28 23:11:44 +0700

    app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ st.subheader("Pembangunan TPQ Al Mutaqien Sidakangen")
 
 # --- KONEKSI GOOGLE SHEETS ---
 # Masukkan URL Google Sheets Anda di sini
-URL_SHEETS = "https://docs.google.com/spreadsheets/d/URL_ANDA_DISINI"
+URL_SHEETS = "https://docs.google.com/spreadsheets/d/1Sl8mx5MhmunKnm_wEP3Wr8f4ddVFzcjtJY8s_UmgcI0/edit?usp=sharing"
 
 try:
     conn = st.connection("gsheets", type=GSheetsConnection)
